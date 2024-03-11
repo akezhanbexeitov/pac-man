ab8f2555d7cce7df0be53cb47041d3a058e1e7f3
Akezhan <akidulatini@gmail.com>
2024-03-11 18:24:52 +0500

    Create signup service function, create example of using it

--- packages/client/src/services/auth.ts
+++ packages/client/src/services/auth.ts
@@ -1,1 +1,24 @@
-// Делаем сервисные функции запросов на сервер для авторизации
+import { useHttp } from "@/hooks/useHttp"
+import { APIError, SignupData, SignupResponse } from "@/typings/responses/auth"
+
+const endpoints = {
+  signup: "/auth/signup",
+  signin: "/auth/signin",
+  me: "/auth/user",
+  logout: "/auth/logout",
+}
+
+const AuthService = () => {
+  const http = useHttp()
+
+  const signup = async (data: SignupData): Promise<SignupResponse | APIError> => {
+    const response = await http.post<SignupResponse>(endpoints.signup, data)
+    return response.data
+  }
+
+  return {
+    signup,
+  }
+}
+
+export default AuthService
--- packages/client/src/typings/responses/auth.ts
+++ packages/client/src/typings/responses/auth.ts
@@ -1,1 +1,20 @@
-// Сюда пишем интерфейсы для ответов от сервера на запросы авторизации
+export type UserDTO = {
+  id: number
+  first_name: string
+  second_name: string
+  display_name: string
+  phone: string
+  login: string
+  avatar: string
+  email: string
+}
+
+export type SignupData = Omit<UserDTO, 'avatar' | 'display_name' | 'id'>  & {
+  password: string
+}
+
+export type SignupResponse = Pick<UserDTO, 'id'>
+
+export type APIError = {
+  reason: string
+}
