5ad018b6a95ecaf3767e59dd82589a30a5708b19
Akezhan <akidulatini@gmail.com>
2024-03-11 19:34:13 +0500

    Create basic HTML and CSS structure

--- packages/client/src/pages/App/index.tsx
+++ packages/client/src/pages/App/index.tsx
@@ -1,6 +1,5 @@
 import '@/styles/index.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Navigation } from '@/components'
 import {
   ForumPage,
   GamePage,
@@ -15,22 +14,19 @@ import {
 
 const App = () => {
   return (
-    <main>
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="registration" element={<RegisterPage />} />
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="game" element={<GamePage />} />
-          <Route path="leaderboard" element={<LeaderboardPage />} />
-          <Route path="forum" element={<ForumPage />} />
-          <Route path="forum/:topicId" element={<TopicPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="registration" element={<RegisterPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="game" element={<GamePage />} />
+        <Route path="leaderboard" element={<LeaderboardPage />} />
+        <Route path="forum" element={<ForumPage />} />
+        <Route path="forum/:topicId" element={<TopicPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- packages/client/src/pages/LoginPage/index.tsx
+++ packages/client/src/pages/LoginPage/index.tsx
@@ -1,5 +1,29 @@
+import { Button, Field } from '@/components/ui'
+import styles from './index.module.scss'
+
 const LoginPage = () => {
-  return <div>LoginPage</div>
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    console.log('Submitted')
+  }
+
+  return (
+    <div className={styles['login__layout']}>
+      <div className={styles['login__background']}>
+        <main className={styles['login__card']}>
+          <h1>Вход</h1>
+
+          <form onSubmit={handleSubmit} className={styles['login__form']}>
+            <Field />
+            <Field />
+            <Button type="primary">Авторизоваться</Button>
+          </form>
+
+          <Button type="secondary">Нет аккаунта?</Button>
+        </main>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
