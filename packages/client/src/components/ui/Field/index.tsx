24adc80c53cbb4baed4f3bcb165bed4f6f8d32de
Akezhan <akidulatini@gmail.com>
2024-03-11 21:33:56 +0500

    Create registration page structure

--- packages/client/src/components/ui/Field/index.tsx
+++ packages/client/src/components/ui/Field/index.tsx
@@ -4,14 +4,20 @@ import styles from './index.module.scss'
 interface Props {
   label: string
   type: HTMLInputTypeAttribute
+  placeholder: string
 }
 
-const Field: FC<Props> = ({ label, type }) => {
+const Field: FC<Props> = ({ label, type, placeholder }) => {
   return (
     <>
       <label className={styles['field__label']} htmlFor={label}>
         <span className={styles['field__text']}>{label}</span>
-        <input className={styles['field__input']} id={label} type={type} />
+        <input
+          className={styles['field__input']}
+          id={label}
+          type={type}
+          placeholder={placeholder}
+        />
       </label>
     </>
   )
