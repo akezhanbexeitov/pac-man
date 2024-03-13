191171219b6f74308a75cadbbbe1603366b64399
Akezhan <akidulatini@gmail.com>
2024-03-13 18:28:47 +0500

    Add arrow icon, modify Head component, add semantic tags, change styles

--- packages/client/src/pages/ForumPage/index.tsx
+++ packages/client/src/pages/ForumPage/index.tsx
@@ -8,31 +8,38 @@ import { useState } from 'react'
 
 const ForumPage = () => {
   const [isOpenModal, setIsOpenModal] = useState(false)
-  
+
   return (
     <div className={styles.wrapper}>
       <main className={styles.content}>
         <div className={styles.forum}>
-          <Head label={'Форум'} route={ROUTES.HOME} />
+          <Head label="Форум" route={ROUTES.HOME} />
           <div className={styles.forum__wrapper}>
             <div className={classnames(styles.forum__block, 'customScroll')}>
               {forums.map(item => (
-                  <Topic name={item.owner} label={item.label} comments={item.comments} key={item.id} id={item.id} />
-                )
-              )}
+                <Topic
+                  name={item.owner}
+                  label={item.label}
+                  comments={item.comments}
+                  key={item.id}
+                  id={item.id}
+                />
+              ))}
             </div>
           </div>
         </div>
-        <Button variant={'primary'} className={styles.btn} onClick={() => setIsOpenModal(true)}>Создать новую
-          тему</Button>
+        <Button
+          variant={'primary'}
+          className={styles.btn}
+          onClick={() => setIsOpenModal(true)}>
+          Создать новую тему
+        </Button>
       </main>
-      <Modal
-        onClose={() => setIsOpenModal(false)}
-        isOpen={isOpenModal}
-      >
+      <Modal onClose={() => setIsOpenModal(false)} isOpen={isOpenModal}>
         <ModalCreate onClose={() => setIsOpenModal(false)} />
       </Modal>
-    </div>)
+    </div>
+  )
 }
 
 export default ForumPage
