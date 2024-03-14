df971e5c3db1a037e86ac50ecc2b5d90b74b046c
Akezhan <akidulatini@gmail.com>
2024-03-14 13:34:23 +0500

    Add ref to scroll to new comment form, add semantic tags

--- packages/client/src/components/Forum/CommentCreate/index.tsx
+++ packages/client/src/components/Forum/CommentCreate/index.tsx
@@ -1,28 +1,43 @@
-import { FC } from 'react'
+import { FC, forwardRef } from 'react'
 import styles from './index.module.scss'
 import { Button } from '@/components/ui'
 
 interface Props {
-  owner: string,
-  date: string,
-  onCancel: () => void;
-  
+  owner: string
+  date: string
+  onCancel: () => void
 }
 
-const CommentCreate: FC<Props> = ({ owner, date, onCancel}) => {
-  return (
-    <div className={styles.comment}>
-      <div className={styles.comment__head}>
-        <div className={styles.comment__name}>{owner}</div>
-        <div className={styles.comment__date}>{date}</div>
-      </div>
-      <textarea className={styles.comment__textarea} placeholder={'Введите ваш коментарий'}/>
-      <div className={styles.comment__btnBlock}>
-        <Button variant={'primary'} className={styles.comment__btnSend}>Отправить</Button>
-        <Button variant={'primary'} className={styles.comment__btnCancel} onClick={onCancel}>Отменить</Button>
-      </div>
-    </div>
-  )
-}
+const CommentCreate = forwardRef<HTMLFormElement, Props>(
+  ({ owner, date, onCancel }, newCommentRef) => {
+    return (
+      <form className={styles.comment} ref={newCommentRef}>
+        <header className={styles.comment__head}>
+          <h5 className={styles.comment__name}>{owner}</h5>
+          <h6 className={styles.comment__date}>{date}</h6>
+        </header>
+        <textarea
+          className={styles.comment__textarea}
+          placeholder="Введите ваш комментарий"
+        />
+        <div className={styles.comment__btnBlock}>
+          <Button
+            variant="primary"
+            type="submit"
+            className={styles.comment__btnSend}>
+            Отправить
+          </Button>
+          <Button
+            variant="primary"
+            type="button"
+            className={styles.comment__btnCancel}
+            onClick={onCancel}>
+            Отменить
+          </Button>
+        </div>
+      </form>
+    )
+  }
+)
 
 export default CommentCreate
