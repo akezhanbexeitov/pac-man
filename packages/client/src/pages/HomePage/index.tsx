94a3508270ac3db497d637896459657d05b9cf4c
Ashot08 <albertguzeev@yandex.ru>
2024-03-20 10:45:30 +0700

    add HomePage

--- packages/client/src/pages/HomePage/index.tsx
+++ packages/client/src/pages/HomePage/index.tsx
@@ -1,13 +1,35 @@
-import '@/styles/index.scss'
-import { Button } from '@/components/ui'
-import { Navigation } from '@/components'
+import styles from './index.module.scss'
+import {Navigation} from '@/components'
+import { useState } from 'react'
+import {Button} from "@/components/ui";
 
 const HomePage = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
   return (
-    <div>
-      <Navigation />
-      <h1>Главная страница игры</h1>
-     
+    <div className={styles.wrapper}>
+
+      <h1 className={styles.title}>YaPacman</h1>
+      <main className={styles.content}>
+        <div className={styles.page}>
+          <div className={styles.pageWrapper}>
+            <div className={styles.pageContent}>
+              <section className={styles.about}>
+                <h2 className={styles.aboutTitle}>Об игре</h2>
+                <p className={styles.aboutText}>
+                  Что-то об игреЧто-то об игреЧто-то об игреЧто-то об игреЧто-то об игреЧто-то об игреЧто-то об игреЧто-то об игреЧто-то об игреЧто-то об игреЧто-то об игреЧто-то об игре
+                </p>
+              </section>
+              <section className={styles.navigation}>
+                <Navigation />
+              </section>
+            </div>
+            <div className={styles.start}>
+              <Button variant="secondary">Start</Button>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
